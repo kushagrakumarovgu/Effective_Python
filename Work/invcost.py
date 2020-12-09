187265a8b18b1b5e6c7a4747107f84753bf154e5
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2020-12-09 17:46:59 +0100

    Addresing review comments.

--- Work/invcost.py
+++ Work/invcost.py
@@ -10,9 +10,9 @@ else:
 def inventory_cost(file_name):
     total = 0.0
     with open(file_name,"rt") as FH:   # read mode for text file.
-        next(FH)
-        csv_reader = csv.reader(FH, delimiter=',')
-        for row in csv_reader:
+        headers = next(FH)
+        list_of_rows = csv.reader(FH, delimiter=',')
+        for row in list_of_rows:
                 quant = int(row[1])
                 price = float(row[2])
                 total += quant * price
