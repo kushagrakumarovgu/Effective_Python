a415260280e601db4ce0ee5f32df21723ddfd015
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2020-12-18 16:54:57 +0100

    Handled case where the order of the columns in the header for different inventory file is not same,.

--- Work/invcost.py
+++ Work/invcost.py
@@ -5,13 +5,14 @@ import sys
 def inventory_cost(file_name):
     total = 0.0
     with open(file_name,"rt") as FH:   # read mode for text file.
-        #headers = next(FH) - NOTE: the headers will be string.
+        #headers = next(FH)  NOTE: the headers will be string.
         list_of_rows = csv.reader(FH, delimiter=',')
         headers = next(list_of_rows) # skip and store the headers. Also, headers will be a list containing each column names.
         for line_no,row in enumerate(list_of_rows,start=1):
             try:
-                quant = int(row[1])
-                price = float(row[2])
+                row_dict = dict(zip(headers,row))
+                quant = int(row_dict['quant'])
+                price = float(row_dict['price'])
                 total += quant * price
             except ValueError:           # ValueError is name of the exception due to file missing.csv
                 print('{} Bad Row: {}'.format(line_no,row))
