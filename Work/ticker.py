999e8413169b8090dc87d777645670395af26b15
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2021-01-19 08:26:49 +0100

    ticker with selected fields only.

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,9 +1,13 @@
 import csv
 from follow import follow
 
+def select_columns(rows,indices):
+    for row in rows:
+        yield [ row[idx] for idx in indices]
 
 def parse_product_data(lines):
     rows = csv.reader(lines)
+    rows = select_columns(rows,[0,1,4])
     return rows
 
 if __name__ == '__main__':
