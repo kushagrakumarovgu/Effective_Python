6466b3befe190dd60af38cb0e400ab5487e023c9
kushagrakumarovgu <64306534+kushagrakumarovgu@users.noreply.github.com>
2021-01-19 08:28:02 +0100

    convert types and create dictionaries.

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,6 +1,15 @@
 import csv
 from follow import follow
 
+
+def convert_types(rows,types):
+    for row in rows:
+        yield [ func(val) for func, val in zip(types,row) ]
+
+def make_dicts(rows,headers):
+    for row in rows:
+        yield dict(zip(headers,row))
+
 def select_columns(rows,indices):
     for row in rows:
         yield [ row[idx] for idx in indices]
@@ -8,6 +17,8 @@ def select_columns(rows,indices):
 def parse_product_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows,[0,1,4])
+    rows = convert_types(rows,[str,float,float])
+    rows = make_dicts(rows,['name','price','change'])
     return rows
 
 if __name__ == '__main__':
